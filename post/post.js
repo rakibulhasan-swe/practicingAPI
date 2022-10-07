2d965b796b1d95281ec3d05562e4f13692d88acb
unknown <rakibulhasan.seu.cse@gmail.com>
2022-10-08 04:26:38 +0600

    displaying post in ui

---
+++ post/post.js
@@ -0,0 +1,17 @@
+document.getElementById("loadPost").addEventListener('click', function loadUsers(){
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(res => res.json())
+    .then(data => displayPost(data))
+})
+const displayPost = (posts) => {
+    const postContainer = document.getElementById("posts");
+    for(const post of posts){
+        const div = document.createElement('div');
+        div.classList.add('card');
+        div.innerHTML = `
+        <h3>${post.title}</h3>
+        <p>${post.body}</p>
+        `;
+        postContainer.appendChild(div);
+    }
+}
