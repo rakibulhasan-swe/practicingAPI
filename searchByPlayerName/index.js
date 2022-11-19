f4b1e959cbadb02b9b96c3748be7f4626497afa5
unknown <rakibulhasan.seu.cse@gmail.com>
2022-11-20 03:52:41 +0600

    error fixing

--- searchByPlayerName/index.js
+++ searchByPlayerName/index.js
@@ -1,5 +1,6 @@
 //https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=neymar
 document.getElementById("err").style.display = 'none';
+// driver function
 document.getElementById('players').addEventListener('click', function(){
     const input = document.getElementById('input');
     const text = input.value;
@@ -15,10 +16,10 @@ document.getElementById('players').addEventListener('click', function(){
         fetch(url)
         .then(res => res.json())
         .then(data => players(data.player))
-        .catch(err => console.log(err))
+        .catch(err => Error(err))
     }
 });
-
+// showing player details
 const players = (data) => {
     const mainDiv = document.getElementById('mainCard');
     mainDiv.textContent = "";
@@ -32,10 +33,9 @@ const players = (data) => {
                 <img src="${player.strThumb}" class="card-img-top w-50 mx-auto" alt="">
                 <div class="card-body">
                     <h5 class="card-title pb-2">${player.strPlayer}</h5>
-                    <p class="card-text">National Team: ${player.strTeam2}</p>
-                    <p class="card-text">Club Team: ${player.strTeam}</p>
+                    <p class="card-text">Club: ${player.strTeam}</p>
                     <p class="card-text">Date of birth: ${player.dateBorn}</p>
-                    <p class="card-text">${player.strDescriptionEN.slice(0, 210)}</p>
+                    <p class="card-text">${player.strDescriptionEN}</p>
                     <a href="#" class="btn btn-outline-dark text-center pt-2">View Details</a>
                 </div>
             </div>
@@ -43,9 +43,9 @@ const players = (data) => {
         mainDiv.appendChild(div);
     })
 };
-
+// showing error for empty and not found players
 const Error = () => {
     const err = document.getElementById("err");
-    err.innerText = "Can't be empty! Try again with players name";
+    err.innerText = "Can't be empty or Not Found! Try again with players name";
     err.style.display = "block";
 }
